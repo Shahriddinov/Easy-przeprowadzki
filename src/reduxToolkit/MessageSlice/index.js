3af0a2fa167e2386563501a7752345078c71b8d1
Shahriddinov <shahriddinovmurod195@gmail.com>
2025-03-22 01:04:31 +0500

    contact message
    Signed-off-by: Shahriddinov <shahriddinovmurod195@gmail.com>

--- src/reduxToolkit/MessageSlice/index.js
+++ src/reduxToolkit/MessageSlice/index.js
@@ -1,10 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import {MESSAGE_SEND} from "../../Services/api/utilis";
+import { MESSAGE_SEND } from "../../Services/api/utilis";
 import { toast } from "react-toastify";
+
 export const sendMessage = createAsyncThunk("message/post", async (sendDataMessage, { rejectWithValue }) => {
     try {
-        const response = await axios.post(MESSAGE_SEND, sendDataMessage);
+        // x-www-form-urlencoded formatiga o‘tkazish
+        const formData = new URLSearchParams();
+        Object.keys(sendDataMessage).forEach((key) => {
+            formData.append(key, sendDataMessage[key]);
+        });
+
+        const response = await axios.post(MESSAGE_SEND, formData, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded", // 🟢 MUHIM!
+            },
+        });
+
         if (response.status === 200) {
             toast.success("Twoja wiadomość została wysłana pomyślnie!", {
                 position: "top-right",
@@ -15,6 +27,6 @@ export const sendMessage = createAsyncThunk("message/post", async (sendDataMessa
         toast.error("Błąd podczas wysyłania wiadomości!", {
             position: "top-right",
         });
-        return rejectWithValue(error.response.data);
+        return rejectWithValue(error.response?.data);
     }
 });
